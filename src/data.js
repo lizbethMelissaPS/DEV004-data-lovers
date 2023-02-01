a60b85d826f45d26fcbb5bf16c143ff64de02998
AlexandraValdez <alexandravaldezs@hotmail.com>
2023-02-01 10:44:43 -0500

    nombre de funciones en data

--- src/data.js
+++ src/data.js
@@ -1,9 +1,26 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
+export const filterData = (data, condition) => {
   return 'example';
 };
 
-export const anotherExample = () => {
+export const sortData = (data, sortBy, sortOrder) => {
   return 'OMG';
 };
+
+export const computeStats = (data) => {
+  return 'OMG';
+};
+
+
+const pokeball = document.querySelector('.pokeball')
+
+function fetchPokemon(id) {
+  fetch('../pokemon.json')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  }
+    )
+
+}
