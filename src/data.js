81ce7960876e618136876bbe8f15f13d91283c64
lizbethMelissaPS <melissasud27@gmail.com>
2023-02-02 17:18:50 -0500

    funcion mostrar

--- src/data.js
+++ src/data.js
@@ -1,26 +1,22 @@
+
 // estas funciones son de ejemplo
+/* esta función filter o filtrar recibiría la data, 
+y nos retornaría aquellos datos que sí cumplan con la condición. */
 
 export const filterData = (data, condition) => {
   return 'example';
 };
-
+/* ordenar recibe tres parámetros. El primer parámetro, 
+data, nos entrega los datos. El segundo parámetro, 
+sortBy, nos dice con respecto a cuál de los campos de la data se quiere ordenar. El tercer parámetro,
+ sortOrder, indica si se quiere ordenar de manera ascendente o descendente. */
 export const sortData = (data, sortBy, sortOrder) => {
-  return 'OMG';
+  return 'ordenar';
 };
-
+/* calcular, nos permitirá hacer cálculos estadísticos básicos
+ para ser mostrados de acuerdo a la data proporcionada. */
 export const computeStats = (data) => {
   return 'OMG';
 };
 
 
-const pokeball = document.querySelector('.pokeball')
-
-function fetchPokemon(id) {
-  fetch('../pokemon.json')
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-  }
-    )
-
-}
