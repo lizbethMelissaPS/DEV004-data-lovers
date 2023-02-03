fcb519247849db58a047cce250d98b2be9b96cd0
AlexandraValdez <alexandravaldezs@hotmail.com>
2023-02-03 17:24:55 -0500

    mostrar types separados

--- src/main.js
+++ src/main.js
@@ -2,44 +2,41 @@ import { filterData, sortData, computeStats } from './data.js';
 //import data from './data/pokemon/pokemon.js';
 
 fetch("./data/pokemon/pokemon.json")
-    .then((res) => {
-        return res.json();
-    })
-    .then((data) => {
-        const arr = data.pokemon
-        /*  */
-        const mostrar = function (arr) {
-            let templateListPoke = '';
-            // recorremos nuestro array con forEach
-            arr.forEach((element) => {
-                // creamos un template(string) por cada elemento del array
-                const card = `
-            <div class='card-link'>
-              <article class='blog-card'>
-                <img class='post-image' src='${element.img}' />
-                <h3 class='post-id'>${element.id}</h3>
-                <div class='article-details'>
-                  <h4 class='post-title'>${element.name}</h4>
-                  <h6 class='post-category'>${element.type}</h6>
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    const arr = data.pokemon
+    const mostrar = function (arr) {
+      let templateListPoke = '';
+      // recorremos nuestro array con forEach
+      arr.forEach((element) => {
+        const listTypes = element.type.map((type) => `<p class="${type} type">${type}</p>`);
+        // creamos un template(string) por cada elemento del array
+        const card = `
+                <div class="pokemon">
+                    <div class="pokeball">
+                        <div class="name-container">
+                            <p class="num">#${element.num}</p>
+                            <p class="name">${element.name}</p>
+                        </div>
+                        <div class="img-container">
+                            <img src='${element.img}' alt="${element.name}"/>
+                        </div>
+                        <div class="type-container">
+                            ${listTypes.join('')}
+                        </div>
+                    </div>
                 </div>
-              </article>
-            </div>
           `;
-                templateListPoke += card;
-            });
-            document.getElementById('allThePokemones').innerHTML = templateListPoke;
-        };
-        mostrar(arr);
-        /*  */
+        templateListPoke += card;
+      });
+      document.getElementById('listaPokemon').innerHTML = templateListPoke;
+    };
+    mostrar(arr);
 
 
-    })
-
-/* for (let i = 0; i < arr.length; i++) {
-  const element = arr[i];
-  console.log(element.num);
-  
-} */
+  })
 
 
 
