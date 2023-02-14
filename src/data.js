61a145045f3c3d57373a2df1fd2cfb50c36f87c6
AlexandraValdez <alexandravaldezs@hotmail.com>
2023-02-14 15:43:31 -0500

    update

--- src/data.js
+++ src/data.js
@@ -3,25 +3,38 @@ export const filterDataType = (data, condition) => {
   return filtroType;
 };
 export const filterDataGeneration = (data, condition) => {
-  const filtroGeneration = data.filter((data) =>
-    data.generation.name.includes(condition)
+  const filtroGeneration = data.filter((data) => data.generation.name.includes(condition)
   );
   return filtroGeneration;
 };
-//sortOrder: ascendente , descendente
-//sortBy: numero, nombre
-export const sortData = (data, sortOrder) => {
-  if (sortOrder === "descending") {
-    console.log("aqui2");
-    const des = data.sort().reverse();
-    return des
-  } else {
-    console.log("aqui");
-    return data.sort();
+
+export const sortData = (data,sortBy1,sortBy2,sortBy3,sortBy4, sortOrder) => {
+  let respuesta;
+  switch (sortOrder) {
+  case sortBy1: respuesta = data.sort((a, b) => a.name.localeCompare(b.name)); break;
+  case sortBy2: respuesta = data.sort((a, b) => b.name.localeCompare(a.name)); break;
+  case sortBy3: respuesta = data.sort((a, b) => a.num.localeCompare(b.num)); break;
+  case sortBy4: respuesta = data.sort((a, b) => b.num.localeCompare(a.num)); break;
   }
+  return respuesta
+
 };
+
+export const buscar = (data,condicion) => {
+  const buscarName = data.filter((data) => data.name.startsWith(condicion));
+  const buscarNum = data.filter((data) => data.num.startsWith(condicion));
+  return [...buscarName,...buscarNum ] ;
+};
+
+/* export const buscarNum = (data,condicion) => {
+  const buscarNum = data.filter((data) => data.num.startsWith(condicion));
+  return buscarNum;
+}; */
+
+
 /* calcular, nos permitirá hacer cálculos estadísticos básicos
  para ser mostrados de acuerdo a la data proporcionada. */
-export const computeStats = (data) => {
+/* export const computeStats = (data) => {
   return "OMG";
-};
+}; */
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {filterDataType,filterDataGeneration,sortData,computeStats} from "./data.js";
+import { filterDataType, filterDataGeneration, sortData, buscar } from "./data.js";
 fetch("./data/pokemon/pokemon.json")
   .then((res) => {
     return res.json();
@@ -13,6 +13,7 @@ fetch("./data/pokemon/pokemon.json")
           (type) => `<p class="${type} type">${type}</p>`
         );
         // creamos un template(string) por cada elemento del array
+
         const card = `
                 <div class="pokeball">
                     <div class="pokemon">
@@ -47,24 +48,57 @@ fetch("./data/pokemon/pokemon.json")
       mostrar(filterDataGeneration(arr, condicion));
     });
 
-    //sortOrder: ascendente , descendente
-    //sortBy: numero, nombre
-    const order = document.getElementById("order");
-    order.addEventListener("change", funOrder);
-    function funOrder() {
-      const sortOrder = order.value;
-      mostrar(sortData(arr, sortOrder))
-    }
+    const listOrder = document.getElementById("order");
+    listOrder.addEventListener("change", () => {
+      const sortOrder = listOrder.value;
+      const sortBy1 = 'a-z'
+      const sortBy2 = 'z-a'
+      const sortBy3 = 'ascending'
+      const sortBy4 = 'descending'
+      mostrar(sortData(arr, sortBy1, sortBy2, sortBy3, sortBy4, sortOrder));
+    });
+
+    const pokeSearch = document.getElementById("pokeSearch");
+    pokeSearch.addEventListener("input", () => {
+      const condicion = pokeSearch.value.toLowerCase();
+      mostrar(buscar(arr, condicion));
+    });
+
   }); //termina el then
 
-const btn = document.getElementById("btn");
-const res = document.getElementById("res");
-btn.addEventListener("click", (e) => {
-  const talla = document.getElementById("idTalla").value;
-  if (talla > 41) {
-    res.innerHTML = "Congrats! You are taller than pikachu";
-  } else {
-    res.innerHTML = "You are shorter than pikachu";
-  }
-  e.preventDefault();
+const menu_toggle = document.querySelector(".menu-toggle");
+const sidebar = document.querySelector('.sidebar');
+
+menu_toggle.addEventListener('click', () => {
+  menu_toggle.classList.toggle('is-active');
+  sidebar.classList.toggle('is-active');
 });
+/* abrir modal */
+// Show Modal superposiciónModal
+const modal = document.getElementById("btnModal");
+const res = document.getElementById("res");
+const superposicionModal = document.getElementById("superposicion");
+
+const mostrarModal = () => {
+  superposicionModal.style.display = 'flex';
+  const talla = document.getElementById("idTalla").value;
+  if (talla > 0.41) {
+
+    const tall = `<h1>Congrats! You are taller than pikachu!</h1>
+    <img src="./images/exito.gif" alt=""/>`
+    res.innerHTML = tall;
+  } else {
+    const short = `<h1>You are shorter than pikachu!</h1>
+    <img src="./images/pikachu-sad.gif" alt=""/>`
+    res.innerHTML = short;
+  }
+}
+modal.addEventListener("click", mostrarModal);
+
+// Hide On Blur ocultarModal
+const ocultarModal = (e) => {
+  if (e.target === e.currentTarget) {
+    superposicionModal.style.display = 'none';
+  }
+}
+superposicionModal.addEventListener("click", ocultarModal)
